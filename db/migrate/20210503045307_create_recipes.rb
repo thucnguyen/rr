dd63d369a8731c3c1544d3ac1e066b1a34c8bd34
Nguyen Van Thuc <thuc.nguyen@trinet.com>
2021-05-03 11:56:49 +0700

    init rails - reactjs project

---
+++ db/migrate/20210503045307_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :description
+      t.string :instruction
+
+      t.timestamps
+    end
+  end
+end
